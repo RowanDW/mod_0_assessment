8d6db6bb82510b758058ea156ba096c50db701de
Rowan DeLong <rowanwinzer@gmail.com>
2021-05-21 11:07:40 -0600

    Add house model and fix laptop model syntax

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,10 +1,10 @@
 laptop_model = {
-  'make' => 'Apple'
-  'model' => 'MacBook Pro'
-  'number_of_keys' => 65
-  'price_in_dollars' => 1299
-  'runs_windows' => false
-  'is_pretty' => true
-  'starting_apps' => ["Safari", "Garage Band", "Terminal"]
-  'cool_features' => ["Touch Id", "Hot corners", "Mission control"]
+  'make' => 'Apple',
+  'model' => 'MacBook Pro',
+  'number_of_keys' => 65,
+  'price_in_dollars' => 1299,
+  'runs_windows' => false,
+  'is_pretty' => true,
+  'starting_apps' => ["Safari", "Garage Band", "Terminal"],
+  'cool_features' => ["Touch Id", "Hot corners", "Mission control"],
 }
---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house_model = {
+  'street_address' => "1234 Pancake ln",
+  'city_state' => 'Denver, CO',
+  'number_of_baths' => 3.5,
+  'number_of_bedrooms' => 5,
+  'is_apartment' = false,
+  'has_ac' = true,
+  'occupants' = ["Wade", "Teresa", "Rowan", "Jules", "Kendall"],
+  'pets' = ["Kovu", "Fiona", "Murphy"],
+}
