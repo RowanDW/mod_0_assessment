0a339d888bbc371270fa96dd59c21aa148166afb
Rowan DeLong <rowanwinzer@gmail.com>
2021-05-21 10:45:30 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+# I'm not sure if there was a typo on the assessment page, so I am going with
+# the exact wording and using 'anmial' as the variable instead of 'animal'
+
+anmial = "Squirrel"
+is_cuddly = true
+anmial = "Pine marten"
+num_8 = 55
+num_3 = 5.0
+quotient = num_3 / num_8
+
+favorite_films = ['The Fast and the Furious', 'Airplane!', 'Aladdin']
+
+favorite_films.shift()
+puts anmial.length()
